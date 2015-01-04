9bd8942c3d68a0126d95520b6f96132eca228e57
Guo Xiang Tan <tgx_world@hotmail.com>
2015-01-04 10:48:53 +0800

    Add Docker script for running Ruby memory benchmarks.

---
+++ ruby_benchmarks/ruby_releases/ruby_memory_benchmarks/bm_io_select3.rb
@@ -0,0 +1,24 @@
+# IO.select performance. a lot of fd
+
+ios = []
+nr = 100
+if defined?(Process::RLIMIT_NOFILE)
+  max = Process.getrlimit(Process::RLIMIT_NOFILE)[0]
+else
+  max = 64
+end
+puts "max fd: #{max} (results not apparent with <= 1024 max fd)"
+
+(max - 10).times do
+  r, w = IO.pipe
+  r.close
+  ios.push w
+end
+
+nr.times do
+  IO.select nil, ios
+end
+
+# We have to close some files otherwise we get an too many files opened error
+# when running ps.
+ios.each { |f| f.close }
