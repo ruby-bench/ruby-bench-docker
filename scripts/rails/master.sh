39ede0efad09a82e44db3beb88d7b573493adab0
bmarkons <mamaveb@gmail.com>
2017-08-09 13:31:32 +0000

    Use docker-compose in runner scripts

--- scripts/rails/master.sh
+++ scripts/rails/master.sh
@@ -16,22 +16,15 @@ PATTERNS=$4
 
 set -x
 
-docker pull rubybench/rails_trunk
+cd $HOME/ruby-bench-docker/rails/master
 
-docker run --name postgres -d postgres:9.6 -c shared_buffers=500MB -c fsync=off -c full_page_writes=off
-docker run --name mysql -e "MYSQL_ALLOW_EMPTY_PASSWORD=yes" -d mysql:5.6.24
-docker run --name redis -d redis:2.8.19
-
-docker run --rm \
-  --link postgres:postgres \
-  --link mysql:mysql \
-  --link redis:redis \
+docker-compose run \
   -e "RAILS_COMMIT_HASH=$COMMIT_HASH" \
   -e "API_NAME=$API_NAME" \
   -e "API_PASSWORD=$API_PASSWORD" \
   -e "MYSQL2_PREPARED_STATEMENTS=1" \
   -e "INCLUDE_PATTERNS=$PATTERNS" \
-  rubybench/rails_trunk
+  rails_master \
+  /bin/bash -l -c "./runner"
 
-docker stop postgres mysql redis
-docker rm -v postgres mysql redis
+docker-compose down
