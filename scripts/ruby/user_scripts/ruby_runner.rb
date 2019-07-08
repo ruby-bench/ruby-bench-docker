efde49645336afc084b60e5c6cf0003de6b5e76b
OsamaSayegh <asooomaasoooma90@gmail.com>
2019-07-08 06:01:23 +0000

    Allow benchmarks in YAML format

--- scripts/ruby/user_scripts/ruby_runner.rb
+++ scripts/ruby/user_scripts/ruby_runner.rb
@@ -7,30 +7,41 @@ file = ARGV[0]
 name = ENV["SCRIPT_NAME"]
 script_url = ENV["SCRIPT_URL"]
 
+if script_url.end_with?(".rb")
+  path = "#{file}.rb"
+elsif script_url.match?(/\.ya?ml$/)
+  path = "#{file}.yml"
+else
+  puts "Unknown extension of #{script_url}"
+  exit 1
+end
+
+File.rename(file, path)
+
 ENV['REPO_NAME'] = 'ruby'
 ENV['ORGANIZATION_NAME'] = 'ruby'
-ENV['BENCHMARK_TYPE_DIGEST'] = Digest::SHA2.file(file).hexdigest
+ENV['BENCHMARK_TYPE_DIGEST'] = Digest::SHA2.file(path).hexdigest
 ENV['BENCHMARK_TYPE_SCRIPT_URL'] = script_url
 ENV['RUBY_ENVIRONMENT'] = 'true'
 ENV['LANG'] = 'C.UTF-8'
 
 ips_command = [
-  "benchmark-driver", file,
+  "benchmark-driver", path,
   "-e", "#{name}::#{RbConfig.ruby.shellescape}",
   "-e", "#{name}_jit::#{RbConfig.ruby.shellescape} --jit",
   "--runner", "ips",
-  "--output", "rubybench", 
-  "--repeat-count", "1", 
+  "--output", "rubybench",
+  "--repeat-count", "1",
   "--repeat-result", "best",
   "--timeout", "60"
 ]
 
 rsskb_command = [
-  "benchmark-driver", file,
+  "benchmark-driver", path,
   "-e", "rss_kb::#{RbConfig.ruby.shellescape}",
   "--runner", "rsskb",
-  "--output", "rubybench", 
-  "--repeat-count", "1", 
+  "--output", "rubybench",
+  "--repeat-count", "1",
   "--repeat-result", "best",
   "--timeout", "60"
 ]
